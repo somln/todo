ca0398b2236f7a77d7051ddbc717756316d8b923
Somin Park <ps4708@naver.com>
2024-08-02 22:07:22 +0900

    Apply database environment variables

--- db/db.go
+++ db/db.go
@@ -3,25 +3,40 @@ package db
 import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 	"todo/models"
 )
 
 var Db *gorm.DB
 
 func InitDB() {
-	dsn := "root:root@tcp(localhost:3307)/local_todo?charset=utf8mb4&parseTime=True&loc=Local"
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
+	user := os.Getenv("MYSQL_USER")
+	password := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("MYSQL_DATABASE")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		user, password, host, port, name)
+
 	var err error
 	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("DB 연결 실패")
+		panic("DB connection failed: " + err.Error())
 	}
 
 	err = Db.AutoMigrate(&models.Todo{})
 	if err != nil {
-		panic("테이블 마이그레이션 실패")
+		panic("Table migration failed: " + err.Error())
 	}
 
-	fmt.Println("DB 연결 성공")
+	fmt.Println("DB connection successful")
 }
